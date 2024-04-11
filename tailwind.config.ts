4564c0b2cec739045e9fc2a1cd2b93dd46af7f05
raymart-iphitech <raymart-iphitech@gmail.com>
2024-04-11 17:13:16 +0800

    add jit mode to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: 'media',
+  mode: "jit",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
