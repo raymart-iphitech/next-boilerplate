e6ba45be1030d78fecf4d890aaa13bc888ef6434
raymart-iphitech <raymart-iphitech@gmail.com>
2024-04-17 10:39:10 +0800

    added query client provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,19 @@
 import "@/styles/globals.css";
+import { 
+  QueryClient, 
+  QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 
 
+export const queryClient = new QueryClient()
+
 const App = ({ Component, pageProps }: AppProps) => {
 
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={ queryClient }>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
 
 
